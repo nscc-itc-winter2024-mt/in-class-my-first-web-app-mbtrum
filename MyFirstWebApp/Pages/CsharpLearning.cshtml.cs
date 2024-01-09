27a3ca7ab7cea118b487660e3a3cfd56374ab831
Michael Trumbull <W0304263@campus.nscc.ca>
2024-01-09 16:19:28 -0400

    Added C# learning.

---
+++ MyFirstWebApp/Pages/CsharpLearning.cshtml.cs
@@ -0,0 +1,92 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyFirstWebApp.Pages
+{
+    public class CsharpLearningModel : PageModel
+    {
+        //
+        // Properties
+        //
+
+        private readonly ILogger<CsharpLearningModel> _logger;
+
+        //
+        // Methods
+        //
+
+        public CsharpLearningModel(ILogger<CsharpLearningModel> logger)
+        {
+            _logger = logger;
+
+            _logger.Log(LogLevel.Information, "Constructor() called!");
+        }
+
+        public void OnGet()
+        {
+            // Logging messages
+            _logger.Log(LogLevel.Information, "OnGet() called!");
+
+            // Data types
+            int myInt = 5;
+            decimal myDecimal = 2.50M;
+            bool myBool = false;
+            string myText = "Hello C#!!!!";
+
+            int myOtherInt;
+            myOtherInt = 20;
+
+            // Operators
+            int sum = myInt + myOtherInt;
+            _logger.Log(LogLevel.Information, "Sum: " + sum);
+
+            // Strings
+            string newText = myText.ToUpper();
+            _logger.Log(LogLevel.Information, "Uppercase text: " + newText);
+
+            // If Statements
+            if(myInt > 5)
+            {
+                _logger.Log(LogLevel.Information, "myInt is greater than 5");
+            }
+            else if(myInt < 5)
+            {
+                _logger.Log(LogLevel.Information, "myInt is less than 5");
+            }
+            else
+            {
+                _logger.Log(LogLevel.Information, "myInt is equal to 5");
+            }
+
+            // Collections
+
+            //int[] myNums = new int[myInt];
+            //int[] myNums = {0,1,2,3,4,5};
+            string[] numStrings = {"hello","world"};
+            string newString = numStrings[0] + numStrings[1];
+            _logger.Log(LogLevel.Information, "Arrays are fun - " + newString);
+
+            List<string> namesList = new List<string>();
+            namesList.Add("Mike");
+            namesList.Add("Joe");
+            namesList.Add("Sue");
+            //namesList.Remove("Sue");
+
+            int numNames = namesList.Count();
+            _logger.Log(LogLevel.Information, "My list has " + numNames + " friends.");
+
+            // Loops (counter, condition, increment)
+            _logger.Log(LogLevel.Information, "My friends (for):");
+            for (int i = 0; i < numNames; i++) 
+            {
+                _logger.Log(LogLevel.Information, namesList.ElementAt(i));
+            }
+
+            _logger.Log(LogLevel.Information, "My friends (foreach):");
+            foreach (string name in namesList)
+            {
+                _logger.Log(LogLevel.Information, name);
+            }
+        }
+    }
+}
