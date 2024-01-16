9dfa3b4b447ca004340020b61a83e6cd5d122036
Michael Trumbull <W0304263@campus.nscc.ca>
2024-01-16 15:40:55 -0400

    Added some more one-way binding.

--- MyFirstWebApp/Pages/RazorLearning.cshtml.cs
+++ MyFirstWebApp/Pages/RazorLearning.cshtml.cs
@@ -16,11 +16,14 @@ namespace MyFirstWebApp.Pages
 
         public string[] pets = { "dog", "cat", "hamster" };
 
+        // Freind object
         public Friend friend = new Friend() { 
             FirstName = "Lando", 
             LastName = "Carlissian", 
             Email = "lando@nscc.ca" 
         };
+
+
 
         //
         // Methods
@@ -31,13 +34,45 @@ namespace MyFirstWebApp.Pages
             _logger = logger;
 
             _logger.Log(LogLevel.Information, "Constructor() called!");
+
+            // Add some addresses
+            List<Address> addresses = new List<Address>();
+            
+            addresses.Add(new Address()
+            {
+                Street = "1 Robie St.",
+                City = "Halifax",
+                Province = "NS",
+                Country = "CA",
+                PostalCode = "B6D4S2"
+            });
+
+            // Another way to add the address
+            Address address2 = new Address();
+            address2.Street = "22 Portland St.";
+            address2.City = "Dartmouth";
+            address2.Province = "NS";
+            address2.Country = "CA";
+            address2.PostalCode = "B9M6X3";
+            addresses.Add(address2);
+
+            friend.Addresses = addresses;
+
+            _logger.Log(LogLevel.Information, "# of addresses= " + addresses.Count());
         }
 
         public void OnGet()
         {
-            _logger.Log(LogLevel.Information, "Onget() called!");
+            _logger.Log(LogLevel.Information, "OnGet() called!");
 
 
         }
+
+        public void OnPost()
+        {
+            _logger.Log(LogLevel.Information, "OnPost() called!");
+        }
+
+
     }
 }
